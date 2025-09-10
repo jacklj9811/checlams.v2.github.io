cfbe755da5dfad74302478d3bfe12df840cc873a
jacklj9811 <49219970+jacklj9811@users.noreply.github.com>
2025-09-10 02:12:06 +0000

    Deploying to gh-pages from @ jacklj9811/checlams.v2.github.io@ea5a7ac299d6e85f356b0ef2eb25d3aa57b95fe4 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "jacklj9811/checlams.v2.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
